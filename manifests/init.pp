ec002db9088881669f92bfd3103e05964299f906
Graham Gilbert <graham@grahamgilbert.com>
2014-05-16 11:42:21 +0100

    Ensure the older version of Dockutil is present, 2.0.0 break things

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,6 @@
 class dockutil{
   repository { 'Dockutil':
+    ensure => '4bbdbc95519b70ac00391c4ebe34592fd351f491',
     source => 'kcrawford/dockutil',
     path   => "${boxen::config::cachedir}/dockutil",
   }
