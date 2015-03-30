9c759d50779e9b34d8fa7647dd73e27751bb1cf1
coreone <teixeira@broadinstitute.org>
2015-03-30 16:05:30 -0400

    Add the option to pass a "version" for dockutil instead of a hard-coded commit hash

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,13 @@
-class dockutil{
+class dockutil (
+  $version = undef
+) {
+
+  unless $version {
+    $version = '4bbdbc95519b70ac00391c4ebe34592fd351f491'
+  }
+
   repository { 'Dockutil':
-    ensure => '4bbdbc95519b70ac00391c4ebe34592fd351f491',
+    ensure => $version,
     source => 'kcrawford/dockutil',
     path   => "${boxen::config::cachedir}/dockutil",
   }
